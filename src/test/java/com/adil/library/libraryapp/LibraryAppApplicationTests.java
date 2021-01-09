7129d895aef0e53b94ed3845f8c7be504d25be41
Adil Saju <adilsaju@gmail.com>
2021-01-09 23:08:54 +0530

    first commit

---
+++ src/test/java/com/adil/library/libraryapp/LibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adil.library.libraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
