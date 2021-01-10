a78e1f592321c2f39c59addf2c4f0e0ff720fc5f
Adil Saju <adilsaju@gmail.com>
2021-01-10 18:14:09 +0530

    h2 conf added

--- src/main/java/com/adil/library/controller/ListBookController.java
+++ src/main/java/com/adil/library/controller/ListBookController.java
@@ -1,5 +1,8 @@
 package com.adil.library.controller;
 
+import com.adil.library.model.Author;
+import com.adil.library.model.Book;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan
 @RestController
 public class ListBookController {
-    
-    @GetMapping("/list")
+
+	// @GetMapping("/")
+	// @ResponseBody
+	// public String index() {
+	// 	return "welcome!";
+	// }
+
+	@GetMapping("/")
 	@ResponseBody
-	public String getBook() {
-		
-		return "hello books";
+	public String listBooks() {
+		Book book1 = new Book();
+		book1.setBname("alchemist");
+
+		Author a1 = new Author();
+		a1.setAname("Paulo Coelho");
+
+		book1.setAuthor(a1);
+		return book1.toString();
 	}
 }
