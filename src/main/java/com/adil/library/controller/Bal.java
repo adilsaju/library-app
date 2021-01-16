83cf73123b80dc9c8834d4bc50df9b017a64d73f
Adil Saju <adilsaju@gmail.com>
2021-01-17 00:15:39 +0530

    new sample mustache controller and template working

---
+++ src/main/java/com/adil/library/controller/Bal.java
@@ -0,0 +1,31 @@
+package com.adil.library.controller;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.adil.library.model.Author;
+import com.adil.library.model.Book;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@ComponentScan
+@RestController
+public class Bal {
+
+    @GetMapping("/article")
+    public ModelAndView displayArticle(Map<String, Object> model) {
+    
+
+    
+        model.put("articles", "abc");
+    
+        return new ModelAndView("index", model);
+    }
+}
